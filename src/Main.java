6f7711d37d509542298180d83c2e323675de7b37
josias-25 <josnkina@gmail.com>
2024-11-14 14:14:24 -0500

    robot

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         Robot myRobot = new Robot();
-        myRobot.move();
-        myRobot.checkBattery();
-        myRobot.stop();
-        myRobot.chargeBattery();
-        myRobot.displayInfo();
+        myRobot.move(); // make the robot move
+        myRobot.checkBattery(); // checking the robot battery.
+        myRobot.stop(); // stopping the robot.
+        myRobot.chargeBattery(); // checking the battery level of the robot.
+        myRobot.displayInfo(); // displays the robot info (color and weight)
     }
 }
